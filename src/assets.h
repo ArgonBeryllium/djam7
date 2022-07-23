65712b1dcf389670e19d10e2f32106b6598506ae
ArBe <argon-beryllium@protonmail.com>
2022-07-23 19:02:04 +0200

    first batch

--- src/assets.h
+++ src/assets.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SDL2/SDL_mixer.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_render.h>
 #include <SDL2/SDL_ttf.h>
 #include <cumt_audio.h>
 #include <cumt_logging.h>
@@ -26,6 +27,7 @@ inline static SDL_Texture* loadTex(const char* path)
 }
 
 inline Mix_Chunk* sfx_shoot;
+inline SDL_Texture *t_test_idle[2], *t_test_hit[2], *t_test_punch[2], *t_test_windup[2], *t_debug, *t_debug_sided[2];
 
 inline static void loadAssets()
 {
@@ -35,4 +37,15 @@ inline static void loadAssets()
 
 	sfx_shoot = loadSFX("res/test.wav");
 	RenderData::loadFont("res/m6x11.ttf", 22);
+
+	t_test_idle[0] = loadTex("res/test_f_idle.png");
+	t_test_idle[1] = loadTex("res/test_b_idle.png");
+	t_test_punch[0] = loadTex("res/test_f_punch.png");
+	t_test_punch[1] = loadTex("res/test_b_punch.png");
+	t_test_hit[0] = loadTex("res/test_f_hit.png");
+	t_test_hit[1] = loadTex("res/test_b_hit.png");
+	t_test_windup[0] = loadTex("res/test_f_windup.png");
+	t_test_windup[1] = loadTex("res/test_b_windup.png");
+	t_debug = loadTex("res/debug.png");
+	t_debug_sided[0] = t_debug_sided[1] = t_debug;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <SDL2/SDL_render.h>
 #include <cstdlib>
 #include <functional>
 #include <iostream>
@@ -5,33 +6,11 @@
 #include <cumt_common.h>
 #include <cumt_render.h>
 #include "assets.h"
+#include "cumt_things.h"
 #include "scenes.h"
 using namespace cumt;
 
-struct Player : Thing2D
-{
-	void update() override
-	{
-		pos += common::inVec()*FD::delta*10;
-		pos.x = std::max(scrToSpace({}).x, std::min(scrToSpace({shitrndr::WindowProps::getWidth(), 0}).x, pos.x));
-	}
-	void render() override
-	{
-		using namespace shitrndr;
-		SetColour({0,0,0,255});
-		v2i p = spaceToScr(pos);
-		FillCircle(p.x, p.y, scl.x/2*getScalar());
-	}
-	void shoot()
-	{
-		audio::play(sfx_shoot);
-		auto* p = parent_set->instantiate(new Thing2D(pos, {.2, 1}));
-		common::act(parent_set, [p]() { p->pos.y -= FD::delta*50; }, .3, [p](){ p->parent_set->scheduleDestroy(p); });
-	}
-};
-Player* player;
-
-struct S_Splash : Scene
+struct SplashScene : Scene
 {
 	void load() override
 	{
@@ -46,27 +25,158 @@ struct S_Splash : Scene
 	}
 	void onKey(SDL_Keycode k) override
 	{
-		Scene::setActive(index+1);
+		if(getActive()==this)
+			Scene::setActive(index+1);
 	}
 };
-struct S_Game : Scene
+
+struct Boxer;
+enum hitDir
 {
+	NONE = 0,
+	LEFT = 1, RIGHT = 2,
+	BOTH = 3
+};
+struct State
+{
+	std::function<State*()> get_next;
+	float dur = 0.5, t = 0;
+	bool interruptable = true;
+	hitDir vuln = BOTH;
+	hitDir dmg = NONE;
+	float remaining() { return dur-t; }
+	float completion() { return t/dur; }
+
+	SDL_Texture** tex = t_debug_sided;
+	Boxer* parent;
+
+	State(Boxer* parent_) : parent(parent_) {}
+
+	virtual void enter() {}
+	virtual void update() {}
+	virtual void exit() {}
+	virtual void interrupt() {}
+};
+struct IdleState : State
+{
+	IdleState(Boxer* parent_) : State(parent_)
+	{
+		get_next = [this]() { return new IdleState(parent); };
+		tex = t_test_idle;
+		vuln = LEFT;
+	}
+};
+struct Boxer : Thing2D
+{
+	State* state = new IdleState(this);
+	Boxer* opponent;
+	bool is_player;
+
+	Boxer(bool is_player_ = false) :
+		Thing2D(is_player_?v2f(0,.1):v2f(0,-.1)), is_player(is_player_) {}
+
+	bool setState(State* next, bool interrupt = true, bool auto_delete = true)
+	{
+		if(interrupt && !state->interruptable)
+		{
+			if(auto_delete)
+				delete next;
+			return false;
+		}
+		if(interrupt) state->interrupt();
+
+		state->exit();
+		delete state;
+		state = next;
+		state->enter();
+
+		return true;
+	}
+	void render() override
+	{
+		using namespace shitrndr;
+		Copy(state->tex[is_player], getRect());
+	}
+	void update() override
+	{
+		state->update();
+		state->t += FD::delta;
+		if(state->t>=state->dur)
+		{
+			setState(state->get_next(), false);
+		}
+	}
+};
+struct HitState : State
+{
+	HitState(Boxer* parent_) : State(parent_)
+	{
+		get_next = [this]() { return new IdleState(parent); };
+		tex = t_test_hit;
+	}
+};
+struct PunchState : State
+{
+	bool hit = 0;
+	float dmg_v = .2;
+	PunchState(Boxer* parent_) : State(parent_)
+	{
+		get_next = [this]() { return new IdleState(parent); };
+		dur = .2;
+		tex = t_test_punch;
+		dmg = LEFT;
+		vuln = NONE;
+		interruptable = false;
+	}
+	void update() override
+	{
+		if(hit) return;
+		if(parent->opponent->state->vuln & dmg)
+		{
+			parent->opponent->setState(new HitState(parent->opponent));
+			hit = true;
+		}
+	}
+};
+struct WindupState : State
+{
+	WindupState(Boxer* parent_, float dur_) : State(parent_)
+	{
+		dur = dur_;
+		get_next = [this]() { return new PunchState(parent); };
+		tex = t_test_windup;
+	}
+};
+
+struct GameScene : Scene
+{
+	Boxer* a, *b;
+	inline Boxer* getP() { return a->is_player?a:b; }
 	void start() override
 	{
-		player = set.instantiate(new Player());
+		a = set.instantiate(new Boxer(true), "boxer A");
+		b = set.instantiate(new Boxer(false), "boxer B");
+		a->opponent = b;
+		b->opponent = a;
 	}
 	void load() override
 	{
 		using namespace shitrndr;
-		bg_col = {245,245,245,255};
-		Thing2D::view_scale = .5;
+		bg_col = {45,25,45,255};
+		Thing2D::view_scale = 4;
+	}
+	void loop() override
+	{
+		set.update();
+		set.render();
+		getP()->render();
 	}
 	void onKey(SDL_Keycode k) override
 	{
 		switch(k)
 		{
 			case SDLK_SPACE:
-				player->shoot();
+				getP()->setState(new WindupState(getP(), .2));
 				break;
 			default: break;
 		}
@@ -77,7 +187,7 @@ void gameSetup()
 {
 	using namespace shitrndr;
 	WindowProps::setLockType(shitrndr::WindowProps::BARS);
-	Scene::scenes = {new S_Splash, new S_Game};
+	Scene::scenes = {new SplashScene, new GameScene};
 	Scene::init();
 
 	loadAssets();
